1b56e18bba625aa72ef3b21c07648b6edfcd6f16
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-11-10 11:08:23 +0300

    added sensor types

---
+++ V1.3__insertSensorTypes.sql
@@ -0,0 +1,7 @@
+INSERT INTO sensor_types (name) VALUES 
+    ('Motion Sensor'),
+    ('Water Leak Sensor'),
+    ('Door/Window Sensor'),
+    ('Blind/Curtain Sensor'),
+    ('Smoke Sensor'),
+    ('Light Control Sensor');
